d60074d5ed87bc53e97968345c50299fb0f9e911
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-25 21:57:34 +0200

    Update emulate-vulkan-lavapipe.sh

--- scripts/2.GPU-drivers/emulate-vulkan-lavapipe.sh
+++ scripts/2.GPU-drivers/emulate-vulkan-lavapipe.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "will not work wth propriaterry nvidia driver"
 read -p "Press Enter to install Vulkan emulatoin for Non vulkan cards, CTRL+c to cancel"
 sudo xbps-install -Sy mesa-vulkan-lavapipe
 sudo xbps-install -Sy mesa-vulkan-lavapipe-32bit
