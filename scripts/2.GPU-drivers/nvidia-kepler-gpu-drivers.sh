de0d48c40085b020b09f9218adbdf7d5ea306023
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-26 23:24:03 +0200

    Update nvidia-kepler-gpu-drivers.sh

--- scripts/2.GPU-drivers/nvidia-kepler-gpu-drivers.sh
+++ scripts/2.GPU-drivers/nvidia-kepler-gpu-drivers.sh
@@ -2,8 +2,8 @@
 echo "For NVIDIA Kepler cards and above (Geforce 600/700)"
 #read -p "Press Enter to install, CTRL+c to cancel"
 
-sudo xbps-install nvidia470 nvidia470-libs-32bit nvidia470-opencl linux-lts linux-lts-headers vulkan-loader vulkan-loader-32bit Vulkan-Headers Vulkan-Tools libspa-vulkan libspa-vulkan-32bit ocl-icd ocl-icd-32bit
-echo "Nvidia470 driver may have issues with kernels newer than 6.12."
+sudo xbps-install nvidia470 nvidia470-libs-32bit nvidia470-opencl linux6.1 linux6.1-headers vulkan-loader vulkan-loader-32bit Vulkan-Headers Vulkan-Tools libspa-vulkan libspa-vulkan-32bit ocl-icd ocl-icd-32bit
+echo "Nvidia470 driver may have issues with kernels newer than 6.6."
 echo "You must select linux 6.1 at startup."
 echo "For GRUB Customizer configuration, I recommend the package: download in octoxbps or via sudo xbps-install grub-customizer"
 
