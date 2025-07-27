6cc2aec6736910b2297fe241c9a81f985d173ccc
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-27 18:52:47 +0200

    Update UNTESTED-mesa-amber.sh

--- scripts/2.GPU-drivers/UNTESTED-mesa-amber.sh
+++ scripts/2.GPU-drivers/UNTESTED-mesa-amber.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo xbps-install -S   gettext flex llvm pkg-config python3-Mako glslang   wayland-devel wayland-protocols libdrm-devel zlib-devel   libXdamage-devel libXxf86vm-devel libffi-devel libva-devel   libvdpau-devel libxshmfence-devel ncurses-devel libclc   libglvnd-devel libxml2-devel lua53-devel elfutils-devel   libarchive-devel clang meson
 git clone https://gitlab.freedesktop.org/mesa/mesa.git
 cd mesa-amber
 mkdir build-amber
