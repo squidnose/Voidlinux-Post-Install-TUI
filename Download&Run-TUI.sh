0de277648ddc7239e7d9ca54ca3bd886bc92913b
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-28 11:05:50 +0200

    Update Download&Run-TUI.sh

--- Download&Run-TUI.sh
+++ Download&Run-TUI.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 konsole --fullscreen -e bash -c '
+sudo xbps-install -Sy git dialog newt&&
 cd /tmp &&
 rm -rf Voidlinux-Post-Install-TUI &&
 git clone https://github.com/squidnose/Voidlinux-Post-Install-TUI.git &&
