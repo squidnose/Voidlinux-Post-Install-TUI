55f289fc3dbd58a365e6e6020d9dcc64cd25a73a
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-28 10:54:14 +0200

    Update Switch-to-AmdGPU-GCN1.sh

--- scripts/2.GPU-drivers/Switch-to-AmdGPU-GCN1.sh
+++ scripts/2.GPU-drivers/Switch-to-AmdGPU-GCN1.sh
@@ -9,10 +9,15 @@ echo "This will disable VGA output from DVI-I adapters.(Is being worked on)"
 echo "Backing up current GRUB configuration..."
 sudo cp /etc/default/grub /etc/default/grub.backup
 
+#Chek if the kernel parameters are allready set
+if grep -q "amdgpu.si_support=1 radeon.si_support=0 amdgpu.cik_support=1 radeon.cik_support=0" "/etc/default/grub"; then
+    echo "Kernel parameters already set. No changes needed."
+else
+
 # Modify GRUB_CMDLINE_LINUX_DEFAULT line
 echo "Modifying GRUB kernel parameters in file /etc/default/grub..."
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="amdgpu.si_support=1 radeon.si_support=0 amdgpu.cik_support=1 radeon.cik_support=0 /' /etc/default/grub
-
+fi
 # Update GRUB
 echo "Updating GRUB..."
 sudo update-grub
