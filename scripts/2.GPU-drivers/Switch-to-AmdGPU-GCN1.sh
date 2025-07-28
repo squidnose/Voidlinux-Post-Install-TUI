1d96f952f1293f3883037ef0850f9fdac012518c
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-28 10:46:19 +0200

    Update and rename Switch-to-AmdGPU-GCN1.txt to Switch-to-AmdGPU-GCN1.sh

---
+++ scripts/2.GPU-drivers/Switch-to-AmdGPU-GCN1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to enable AMDGPU driver for older GCN1 cards
+
+echo "This script will enable AMDGPU for older GCN1 cards."
+echo "This will disable VGA output from DVI-I adapters.(Is being worked on)"
+
+# Back up the current GRUB config
+echo "Backing up current GRUB configuration..."
+sudo cp /etc/default/grub /etc/default/grub.backup
+
+# Modify GRUB_CMDLINE_LINUX_DEFAULT line
+echo "Modifying GRUB kernel parameters in file /etc/default/grub..."
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="amdgpu.si_support=1 radeon.si_support=0 amdgpu.cik_support=1 radeon.cik_support=0 /' /etc/default/grub
+
+# Update GRUB
+echo "Updating GRUB..."
+sudo update-grub
+
+# Done
+echo "Done. Please reboot your system for changes to take effect."
