27aa64c90e6f603f8a0ea4728e539e592aa3149b
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-30 04:01:57 +0200

    Delete scripts/2.GPU-drivers/amd-Terrascale-gpu-drivers.sh

--- scripts/2.GPU-drivers/amd-Terrascale-gpu-drivers.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-echo "For AMD/ATI Terrascale 1,2 and 3 cards (Radeon HD 2000 - 6000)"
-#read -p "Press Enter to install, CTRL+c to cancel"
-sudo xbps-install mesa-dri mesa-dri-32bit mesa-vaapi mesa-vaapi-32bit mesa-vdpau mesa-vdpau-32bit mesa-opencl mesa-opencl-32bit gamemode xf86-video-ati ocl-icd ocl-icd-32bit
