ac942d9065187411d828f4560dda8cbe49f96380
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-30 04:01:15 +0200

    Update and rename amd-RDNA-gpu-drivers.sh to amd-AMDVLK.sh

--- scripts/2.GPU-drivers/amd-RDNA-gpu-drivers.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-echo "For AMD RDNA cards and above. AMDVLK Vulkan (RX 5000+)"
-#read -p "Press Enter to install, CTRL+c to cancel"
-sudo xbps-install mesa-dri mesa-dri-32bit mesa-vaapi mesa-vaapi-32bit mesa-vdpau mesa-vdpau-32bit mesa-opencl mesa-opencl-32bit gamemode amdvlk amdvlk-32bit xf86-video-amdgpu vulkan-loader vulkan-loader-32bit Vulkan-Headers Vulkan-Tools radeontop libspa-vulkan libspa-vulkan-32bit ocl-icd ocl-icd-32bit
-
