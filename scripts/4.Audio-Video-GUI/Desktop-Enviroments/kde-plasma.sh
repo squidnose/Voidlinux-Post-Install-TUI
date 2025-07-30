99f44da61d6a98e9eb8396ca3f63a9fca6865d88
squidnose <163349355+squidnose@users.noreply.github.com>
2025-07-30 05:58:23 +0200

    Update kde-plasma.sh

--- scripts/4.Audio-Video-GUI/Desktop-Enviroments/kde-plasma.sh
+++ scripts/4.Audio-Video-GUI/Desktop-Enviroments/kde-plasma.sh
@@ -2,13 +2,16 @@
 echo "Install KDE plasma and KDE apps?"
 #read -p "Press Enter to install, CTRL+c to cancel"
 echo "Installing KDE Plasma, Network manager(Ready for eduroam):"
-sudo xbps-install kde-plasma kde-baseapps xorg wayland plasma-integration sddm dbus NetworkManager python3-dbus
+sudo xbps-install -Syu kde-plasma kde-baseapps xorg wayland plasma-integration sddm dbus NetworkManager python3-dbus
 
 echo "Installing KDE integrations: Google-drive, Browser Inegration, Firewall Information gathering(Local):"
-sudo xbps-install kio-gdrive plasma-integration ufw plasma-firewall clinfo qt6-wayland-tools aha
+sudo xbps-install -Syu kio-gdrive plasma-integration ufw plasma-firewall clinfo qt6-wayland-tools aha
 
 echo "Install kde apps: Notepad, Terminal, Files, Help-Center; Screenshots; Unzip, Unrar; Paint; Bulk Renamer; System Password Manager;Disk space analyzer; App Store for flatpak and Kde addons; GUI package manager for XBPS"
-sudo xbps-install kde-baseapps spectacle ark 7zip-unrar kolourpaint krename kwalletmanager filelight discover octoxbps
+sudo xbps-install -Syu kde-baseapps spectacle ark 7zip-unrar kolourpaint krename kwalletmanager filelight discover octoxbps
+
+echo "installing file previews. To configure it, go to Configure Dolphin -> General -> Previews." 
+sudo xbps-install -Syu kdegraphics-thumbnailers ffmpegthumbs
 
 echo "Enabling services:"
 sudo ln -s /etc/sv/dbus/ /var/service
