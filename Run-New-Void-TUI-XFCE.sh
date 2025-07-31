159faa80a3677126d7707829194ecd1d99af8451
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-01 01:48:40 +0200

    Add files via upload

---
+++ Run-New-Void-TUI-XFCE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+xfce4-terminal --fullscreen -e "bash -c '
+sudo xbps-install -Sy git dialog newt&&
+cd /tmp &&
+rm -rf Voidlinux-Post-Install-TUI &&
+git clone https://github.com/squidnose/Voidlinux-Post-Install-TUI.git &&
+cd Voidlinux-Post-Install-TUI &&
+chmod +x Void-post-install-script.sh &&
+./Void-post-install-script.sh
+'"
