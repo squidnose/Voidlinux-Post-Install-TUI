b52cc9a60c575b38be5141878615d197f283facf
squidnose <ozfiala@gmail.com>
2025-08-05 19:15:17 +0200

    Added the main star of the show:)

---
+++ scripts/4.Audio-Video-GUI/Audio/pipewire-autostart.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+echo "this will add a .desktop file to make pipewire automaticly start for the logged in user that is running this script"
+cat > ~/.config/autostart/wireplumber.desktop <<EOF
+[Desktop Entry]
+Exec=pipewire
+Icon=
+Name=pipewire
+Path=
+Terminal=False
+Type=Application
+EOF
+echo "Done"
