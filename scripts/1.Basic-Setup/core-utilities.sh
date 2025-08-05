0e755d18575586c18c469f8706ccdeaec0d54dee
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-05 10:46:35 +0200

    Fixed Space

--- scripts/1.Basic-Setup/core-utilities.sh
+++ scripts/1.Basic-Setup/core-utilities.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 echo "basic comandline stuff"
-sudo xbps-install -Syu git wget curl nano ncdu fastfetch  htop tmux btop nvme cli
+sudo xbps-install -Syu git wget curl nano ncdu fastfetch htop tmux btop nvme cli
 
 
 
