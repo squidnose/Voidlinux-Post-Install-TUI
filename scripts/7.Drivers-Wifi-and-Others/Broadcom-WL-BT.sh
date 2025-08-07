05da7566c09c926782c100b4c62cc5da118a9c08
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-07 23:23:30 +0200

    I HATE THE PRORPIATERY BROADCOM DRIVERS!!!!!!
    At least they work... kinda

--- scripts/7.Drivers-Wifi-and-Others/Broadcom-WL-BT.sh
+++ scripts/7.Drivers-Wifi-and-Others/Broadcom-WL-BT.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 sudo xbps-install -Syu broadcom-wl-dkms broadcom-bt-firmware
 sudo usermod -aG network "$USER"
-#making a rc file to start on boot
-sudo mkdir -p /etc/rc.local.d/
-sudo tee /etc/rc.local.d/reload-wl.sh >/dev/null <<'EOF'
-#!/bin/sh
-# Wait a few seconds to make sure everything is initialized
-sleep 2
-modprobe -r wl
-modprobe wl
-exit 0
-EOF
 
-sudo chmod +x /etc/rc.local.d/reload-wl.sh
-sudo tee /etc/rc.local.d/reload-wl.sh >/dev/null <<'EOF'
-./etc/rc.local.d/reload-wl.sh
-EOF
+echo"enabling wl..."
+sudo modprobe -r wl
+sleep 5
+sudo modprobe wl
 
-#Test without reboot
-sudo /etc/rc.local.d/reload-wl.sh
-lsmod | grep wl
-nmcli dev status
+echo"Enableed broadcom drivers once"
+echo"After reboot run BCM-fix-wl.sh"
+
 
