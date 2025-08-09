0a2a0f0441afefe88565a6620422fb2dd719ecde
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-09 16:03:48 +0200

    Added Gnome extensions

--- scripts/4.Audio-Video-GUI/Desktop-Enviroments/gnome.sh
+++ scripts/4.Audio-Video-GUI/Desktop-Enviroments/gnome.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo xbps-install -Syu xorg wayland gnome gnome-apps gnome-bluetooth gnome-online-accounts gnome-authenticator gnome-autoar gnome-browser-connector gnome-icon-theme gnome-icon-theme-extras gnome-tweaks gnome-core gnome-shell-extensions gnome-software
+flatpak install -y flathub org.gnome.Extensions
