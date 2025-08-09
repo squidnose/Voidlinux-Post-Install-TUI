eed266ed98df3b30d1dc6a8a47c4418842115596
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-09 16:09:49 +0200

    Update gnome.sh with More Extensiones

--- scripts/4.Audio-Video-GUI/Desktop-Enviroments/gnome.sh
+++ scripts/4.Audio-Video-GUI/Desktop-Enviroments/gnome.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sudo xbps-install -Syu xorg wayland gnome gnome-apps gnome-bluetooth gnome-online-accounts gnome-authenticator gnome-autoar gnome-browser-connector gnome-icon-theme gnome-icon-theme-extras gnome-tweaks gnome-core gnome-shell-extensions gnome-software
-flatpak install -y flathub org.gnome.Extensions
+echo "Installing Core apps"
+sudo xbps-install -Syu xorg wayland gnome gnome-apps gnome-core gnome-software 
+echo "Adding extras"
+sudo xbps-install -Syu gnome-bluetooth gnome-online-accounts gnome-authenticator gnome-autoar gnome-browser-connector gnome-icon-theme gnome-icon-theme-extras 
+echo "adding extensiones"
+sudo xbps-install -Syu gnome-tweaks gnome-shell-extensions extension-manager nemo-extensions nautilus-gnome-terminal-extension nautilus-papers-extension
+install -y flathub org.gnome.Extensions
