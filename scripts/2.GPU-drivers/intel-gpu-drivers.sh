7bf35ce1b347f6be27176326f6f49b7c2b9d2122
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-12 13:16:47 +0200

    Improove Musl support intel-gpu-drivers.sh

--- scripts/2.GPU-drivers/intel-gpu-drivers.sh
+++ scripts/2.GPU-drivers/intel-gpu-drivers.sh
@@ -1,6 +1,13 @@
 #/bin/bash
 echo "For Intel cards Gen 5 and above (Intel HD 2000+)"
 echo "May have issues with Intel Gen 1-4 (GMA and similar). Solution is mesa-amber (Archlinux) or Debian Linux."
-#read -p "Press Enter to install, CTRL+c to cancel"
-sudo xbps-install mesa-dri mesa-dri-32bit mesa-vaapi mesa-vaapi-32bit mesa-vdpau mesa-vdpau-32bit mesa-opencl mesa-opencl-32bit gamemode mesa-vulkan-intel mesa-vulkan-intel-32bit intel-video-accel xf86-video-intel vulkan-loader vulkan-loader-32bit Vulkan-Headers Vulkan-Tools libspa-vulkan libspa-vulkan-32bit ocl-icd ocl-icd-32bit
 
+echo "Installing 3D accelaration packages"
+sudo xbps-install -Syu mesa-dri mesa-opencl gamemode mesa-vulkan-intel xf86-video-intel vulkan-loader Vulkan-Headers Vulkan-Tools ocl-icd 
+echo "Installing Video accelaration packages"
+sudo xbps-install -Syu mesa-vaapi mesa-vdpau intel-video-accel libspa-vulkan libva
+
+echo "Installing 32bit 3D accelaration packages, Will fail on Musl"
+sudo xbps-install -Syu mesa-dri-32bit mesa-opencl-32bit mesa-vulkan-intel-32bit vulkan-loader-32bit ocl-icd-32bit
+echo "Installing 32bit Video accelaration packages, Will fail on Musl"
+sudo xbps-install -Syu mesa-vaapi-32bit mesa-vdpau-32bit libspa-vulkan-32bit libva-32bit
