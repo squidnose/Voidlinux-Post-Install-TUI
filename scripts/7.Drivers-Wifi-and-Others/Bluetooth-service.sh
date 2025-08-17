dbd10fc51be46ae5bc2c0e3b89899417efd87755
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-17 22:43:19 +0200

    Update Bluetooth-service.sh
    Usergrops error

--- scripts/7.Drivers-Wifi-and-Others/Bluetooth-service.sh
+++ scripts/7.Drivers-Wifi-and-Others/Bluetooth-service.sh
@@ -2,6 +2,6 @@
 echo "Installiing Bluetooth related deps: bluez bluez-alsa libspa-bluetooth"
 sudo xbps-install -Syu bluez bluez-alsa libspa-bluetooth
 echo "Adding user to bluetooth group"
-sudo usermod -aG bluetooth
+sudo usermod -aG bluetooth $USER
 echo "enabling bluetooth serice
 sudo ln -s /etc/sv/bluetoothd /var/service/
