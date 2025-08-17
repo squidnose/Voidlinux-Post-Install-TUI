0808ca62cf1e688648c55f511dddf08e9c1eaada
squidnose <163349355+squidnose@users.noreply.github.com>
2025-08-17 22:51:57 +0200

    bad comment out Bluetooth-service.sh

--- scripts/7.Drivers-Wifi-and-Others/Bluetooth-service.sh
+++ scripts/7.Drivers-Wifi-and-Others/Bluetooth-service.sh
@@ -3,5 +3,5 @@ echo "Installiing Bluetooth related deps: bluez bluez-alsa libspa-bluetooth"
 sudo xbps-install -Syu bluez bluez-alsa libspa-bluetooth
 echo "Adding user to bluetooth group"
 sudo usermod -aG bluetooth $USER
-echo "enabling bluetooth serice
+echo "enabling bluetooth serice"
 sudo ln -s /etc/sv/bluetoothd /var/service/
