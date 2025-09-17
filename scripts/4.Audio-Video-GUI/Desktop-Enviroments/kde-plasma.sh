c781495a9f5e3450102ec9b89872bcf071a19413
squidnose <163349355+squidnose@users.noreply.github.com>
2025-09-17 11:39:09 +0200

    added kdepim

--- scripts/4.Audio-Video-GUI/Desktop-Enviroments/kde-plasma.sh
+++ scripts/4.Audio-Video-GUI/Desktop-Enviroments/kde-plasma.sh
@@ -8,8 +8,8 @@ sudo xbps-install -Sy kio-gdrive plasma-browser-integration ufw plasma-firewall 
 echo "Install kde apps: Notepad, Terminal, Files, Help-Center; Screenshots; Unzip, Unrar; Paint; Bulk Renamer; System Password Manager;Disk space analyzer; App Store for flatpak and Kde addons; GUI package manager for XBPS"
 sudo xbps-install -Sy spectacle ark 7zip-unrar kolourpaint krename kwalletmanager filelight discover octoxbps flatpak-kcm
 
-echo "Installing calendar Integrations and kdeconnect"
-sudo xbps-install -Sy kontact calendarsupport akonadi-calendar kdeconnect
+echo "Installing calendar Integrations and kdeconnect kdepim"
+sudo xbps-install -Sy kontact calendarsupport akonadi-calendar kdepim-addons kdepim-runtime kdeconnect
 
 echo "installing file previews. To configure it, go to Configure Dolphin -> General -> Previews." 
 sudo xbps-install -Sy kdegraphics-thumbnailers ffmpegthumbs
