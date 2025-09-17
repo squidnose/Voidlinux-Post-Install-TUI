526ad2aa136a743b7952f995466343bcd4e967c4
squidnose <163349355+squidnose@users.noreply.github.com>
2025-09-17 11:04:07 +0200

    added calendar and akonadi

--- scripts/4.Audio-Video-GUI/Desktop-Enviroments/kde-plasma.sh
+++ scripts/4.Audio-Video-GUI/Desktop-Enviroments/kde-plasma.sh
@@ -1,15 +1,18 @@
 #/bin/bash
 echo "Installing KDE Plasma, Network manager(Ready for eduroam):"
-sudo xbps-install -Syu kde-plasma kde-baseapps xorg wayland plasma-integration sddm dbus NetworkManager python3-dbus plasma-browser-integration
+sudo xbps-install -Sy kde-plasma kde-baseapps xorg wayland plasma-integration sddm dbus NetworkManager python3-dbus plasma-browser-integration
 
 echo "Installing KDE integrations: Google-drive, Browser Inegration, Firewall Information gathering(Local):"
-sudo xbps-install -Syu kio-gdrive plasma-browser-integration ufw plasma-firewall clinfo qt6-wayland-tools aha drkonqi
+sudo xbps-install -Sy kio-gdrive plasma-browser-integration ufw plasma-firewall clinfo qt6-wayland-tools aha drkonqi
 
 echo "Install kde apps: Notepad, Terminal, Files, Help-Center; Screenshots; Unzip, Unrar; Paint; Bulk Renamer; System Password Manager;Disk space analyzer; App Store for flatpak and Kde addons; GUI package manager for XBPS"
-sudo xbps-install -Syu spectacle ark 7zip-unrar kolourpaint krename kwalletmanager filelight discover octoxbps
+sudo xbps-install -Sy spectacle ark 7zip-unrar kolourpaint krename kwalletmanager filelight discover octoxbps
+
+echo "Installing calendar Integrations"
+sudo xbps-install -Sy kontact calendarsupport akonadi-calendar
 
 echo "installing file previews. To configure it, go to Configure Dolphin -> General -> Previews." 
-sudo xbps-install -Syu kdegraphics-thumbnailers ffmpegthumbs
+sudo xbps-install -Sy kdegraphics-thumbnailers ffmpegthumbs
 
 echo "installing Themes for: Cursor, Icons" 
-sudo xbps-install -Syu breeze-cursors breeze
+sudo xbps-install -Sy breeze-cursors breeze
