08bd8b14d4e27ae1bcd3b12d29a5e9dcc5a325bb
squidnose <163349355+squidnose@users.noreply.github.com>
2025-09-20 10:26:28 +0200

    Update utils-disks.sh

--- scripts/1.Basic-Setup/utils-disks.sh
+++ scripts/1.Basic-Setup/utils-disks.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 echo "disk util stuff(NTFS, exfat)"
-sudo xbps-install -Syu exfat-utils exfatprogs ntfs-3g nvme-cli gsmartcontrol
+sudo xbps-install -Syu exfatprogs ntfs-3g nvme-cli gsmartcontrol
 
 
 
