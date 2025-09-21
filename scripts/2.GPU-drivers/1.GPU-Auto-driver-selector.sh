27a2bafaf955544f0818822acb7d80de7db9b904
squidnose <ozfiala@gmail.com>
2025-09-21 23:24:11 +0200

    Auto installer fix for nvidia

--- scripts/2.GPU-drivers/1.GPU-Auto-driver-selector.sh
+++ scripts/2.GPU-drivers/1.GPU-Auto-driver-selector.sh
@@ -14,22 +14,22 @@ echo "Detected GPU: $gpu_info"
 nvidia_fix() {
 
 if whiptail --title "$TITLE" --yesno "Set DRM modeset for Better Wayland support?" 10 60; then
-    bash "$BASE_DIR/Nvidia-FIX-DRM-modeset.sh"
+    bash "$SCRIPT_DIR/Nvidia-FIX-DRM-modeset.sh"
     echo "Ran Nvidia-FIX-DRM-modeset.sh"
 fi
 
 if whiptail --title "$TITLE" --yesno "Set Nvidia GPU for primary display?(Reccomended for hybrid laptop graphics)" 10 60; then
-    bash "$BASE_DIR/Nvidia-FIX/Nvidia-FIX-Only-Hybrid-Setup.sh"
+    bash "$SCRIPT_DIR/Nvidia-FIX/Nvidia-FIX-Only-Hybrid-Setup.sh"
     echo "Ran Nvidia-FIX-Only-Hybrid-Setup.sh"
 fi
 
 if whiptail --title "$TITLE" --yesno "Fix Sleep mode with Nvidia (May reboot system)" 10 60; then
-    bash "$BASE_DIR/Nvidia-FIX/Nvidia-FIX-Suspend.sh"
+    bash "$SCRIPT_DIR/Nvidia-FIX/Nvidia-FIX-Suspend.sh"
     echo "Nvidia-FIX-Suspend.sh"
 fi
 
 if whiptail --title "$TITLE" --yesno "Fix Brighness Controll on Some laptops with Nvidia Only GPUS?" 10 60; then
-    bash "$BASE_DIR/Nvidia-FIX/Nvidia-FIX-Brightness-Controll.sh"
+    bash "$SCRIPT_DIR/Nvidia-FIX/Nvidia-FIX-Brightness-Controll.sh"
     echo "Nvidia-FIX-Brightness-Controll-Nvidia.sh"
 fi
 
