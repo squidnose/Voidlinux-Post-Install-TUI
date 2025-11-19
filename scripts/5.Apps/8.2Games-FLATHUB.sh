6c3ff89fe1e2108d2c899219f216b8228e3ee5a0
squidnose <ozfiala@gmail.com>
2025-11-19 09:33:12 +0100

    Transitionnig to LBAC

---
+++ scripts/5.Apps/8.2Games-FLATHUB.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+# Linux Bulk App Chooser (LBAC)
+# https://codeberg.org/squidnose-code/Linux-Bulk-App-Chooser
+#==================================== Configs ====================================
+## Package list:
+PACKAGES=(PrismLauncher openjdk8-jre openjdk17-jre openjdk21-jre steam steamguard-cli protontricks MangoHud MangoHud-32bit luanti kpat)
+## Manual list entries:
+## "TAG" "DESCRIPTION" "OFF/ON"
+MANUAL_OPTIONS=(
+    "PrismLauncher"      "Custom launcher for Minecraft" OFF
+    "openjdk8-jre"       "Java 8 for Minecraft 1.16.5 and older" OFF
+    "openjdk17-jre"      "Java 17 for Minecraft 1.17 - 1.20.4" OFF
+    "openjdk21-jre"      "Java 21 for Minecraft 1.20.5 and higher" OFF
+    "steam"              "GLIBC ONLY! Digital distribution client bootstrap package - Valve's steam client" OFF
+    "steamguard-cli"     "Utility for Steam 2FA and for managing trade confirmations" OFF
+    "protontricks"       "Simple wrapper that does winetricks things for Proton enabled games" OFF
+    "MangoHud"           "Vulkan and OpenGL overlay for monitoring FPS, temperatures and more" OFF
+    "MangoHud-32bit"     "GLIBC ONLY! 32-bit version of mangohud" OFF
+    "luanti"             "Voxel game-creation platform with easy modding and game creation" OFF
+    "kpat"               "Relaxing card sorting game - Kdes solitare" OFF
+)
+## OFF/ON refers if the menu item will be automaticly selected(ON) or de-selected(OFF)
+
+## Commands:
+INSTALL="sudo xbps-install -Su"
+REMOVE="sudo xbps-remove"
+RECONFIGURE="sudo xbps-reconfigure"
+FORCE_RECONFIGURE="sudo xbps-reconfigure --force"
+#==================================== Funtions ====================================
+manual_selection_menu() {
+    whiptail --title "Manual Package Selection" \
+        --checklist "Choose applications to install:" \
+        25 75 15 \
+        "${MANUAL_OPTIONS[@]}" \
+        3>&1 1>&2 2>&3
+
+}
+#==================================== Main Menu ====================================
+CHOICE=$(whiptail --title "Linux Bulk App Chooser" --menu "Choose an installation mode:" \
+    20 60 10 \
+    1 "Install All" \
+    2 "Manual Selection" \
+    3 "Un-install selected" \
+    4 "Reconfigure Selected" \
+    5 "Force Reconfigure" \
+    3>&1 1>&2 2>&3)
+
+case "$CHOICE" in
+    1)
+        echo "Installing All Packages: ${PACKAGES[*]}"
+        $INSTALL "${PACKAGES[@]}"
+        ;;
+    2)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        ## The RAW output has " that package managers dont like
+        ## The following commands remove quotes and convert into clean array
+        RAW=${RAW//\"/}
+        ## for reference: ${RAW//pattern/replacement}
+        ## explanation:
+            ### // replace for all occurrences
+            ### \" the patern. The backslash is used inside quotes to prevent confusion in the script. Effectively this pattern is "
+
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Install Packages
+        echo "Installing Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $INSTALL "${SELECTED_PACKAGES[@]}"
+        ;;
+    3)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        RAW=${RAW//\"/}
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Remove Packages
+        echo "Removing Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $REMOVE ${SELECTED_PACKAGES[@]}
+        ;;
+    4)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        RAW=${RAW//\"/}
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Reconfigures Packages
+        echo "Removing Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $RECONFIGURE ${SELECTED_PACKAGES[@]}
+    ;;
+    5)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        RAW=${RAW//\"/}
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Forcefully Reconfigures Packages
+        echo "Removing Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $FORCE_RECONFIGURE ${SELECTED_PACKAGES[@]}
+    ;;
+    *)
+        echo "Exiting."
+        exit 0
+        ;;
+esac
+#just in case:)
+exit 0
