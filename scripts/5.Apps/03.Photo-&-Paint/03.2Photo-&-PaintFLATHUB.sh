05badd01abc6457634f559467e5f13b6e5b9a6a5
squidnose <ozfiala@gmail.com>
2025-11-19 17:15:21 +0100

    Photo and Paint is now LBAC plus Fixes

---
+++ scripts/5.Apps/03.Photo-&-Paint/03.2Photo-&-PaintFLATHUB.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+# Linux Bulk App Chooser (LBAC)
+# https://codeberg.org/squidnose-code/Linux-Bulk-App-Chooser
+#==================================== Configs ====================================
+##Title Name
+TITLE="Photo-&-Paint Flatpak"
+## Package list:
+PACKAGES=(io.gitlab.adhami3310.Converter org.gimp.GIMP org.kde.krita org.kde.kolourpaint org.tuxpaint.Tuxpaint)
+## Manual list entries:
+## "TAG" "DESCRIPTION" "OFF/ON"
+MANUAL_OPTIONS=(
+    "io.gitlab.adhami3310.Converter"        "Switchero - Convert Images into diferent formats" OFF
+    "org.gimp.GIMP"                         "GNU image manipulation program" OFF
+    "org.kde.krita"                         "Painting and image editing program" OFF
+    "org.kde.kolourpaint"                   "Free, easy-to-use paint program for KDE" OFF
+    "org.tuxpaint.Tuxpaint"                 "Free, award-winning drawing program for children ages 3 to 12" OFF
+)
+## OFF/ON refers if the menu item will be automaticly selected(ON) or de-selected(OFF)
+
+## Commands:
+INSTALL="flatpak install"
+REMOVE="flatpak remove"
+REPAIR="flatpak repair"
+SHOW_PERMISION="flatpak permission-show"
+#==================================== Show Package List ====================================
+## Build a readable list for whiptail
+PACKAGE_LIST="Available Packages:\n\n"
+
+i=0 ##Index helper
+while [ $i -lt ${#MANUAL_OPTIONS[@]} ]; do
+##MANUAL_OPTIONS Now serves as the number of elements in the array
+##-lt is less than
+    NAME="${MANUAL_OPTIONS[$i]}"
+    DESC="${MANUAL_OPTIONS[$i+1]}"
+
+    PACKAGE_LIST+="$NAME  -  $DESC\n"
+
+    i=$((i+3)) ##is set to 3 because each line has 3 items
+done
+
+## Display the message box
+whiptail --title "$TITLE" --msgbox "$PACKAGE_LIST" 30 80
+
+#==================================== Funtions ====================================
+manual_selection_menu() {
+    whiptail --title "Manual Package Selection for $TITLE" \
+        --checklist "Choose applications to install/un-install/repair:" \
+        25 106 15 \
+        "${MANUAL_OPTIONS[@]}" \
+        3>&1 1>&2 2>&3
+
+}
+#==================================== Main Menu ====================================
+CHOICE=$(whiptail --title "$TITLE" --menu "Choose an installation mode:" \
+    20 60 10 \
+    1 "Install All" \
+    2 "Manual Selection" \
+    3 "Un-install selected" \
+    4 "Repair Selected" \
+    5 "Show Permissions" \
+    3>&1 1>&2 2>&3)
+
+case "$CHOICE" in
+    1)
+        echo "Installing All Packages: ${PACKAGES[*]}"
+        $INSTALL "${PACKAGES[@]}"
+        ;;
+    2)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        ## The RAW output has " that package managers dont like
+        ## The following commands remove quotes and convert into clean array
+        RAW=${RAW//\"/}
+        ## for reference: ${RAW//pattern/replacement}
+        ## explanation:
+            ### // replace for all occurrences
+            ### \" the patern. The backslash is used inside quotes to prevent confusion in the script. Effectively this pattern is "
+
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Install Packages
+        echo "Installing Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $INSTALL "${SELECTED_PACKAGES[@]}"
+        ;;
+    3)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        RAW=${RAW//\"/}
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Remove Packages
+        echo "Removing Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $REMOVE ${SELECTED_PACKAGES[@]}
+        ;;
+    4)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        RAW=${RAW//\"/}
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Repair Packages
+        echo "Reconfigure Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $REPAIR ${SELECTED_PACKAGES[@]}
+    ;;
+    5)
+        RAW=$(manual_selection_menu)
+        if [ -z "$RAW" ]; then
+            echo "No selections made. Exiting."
+            exit 0
+        fi
+        RAW=${RAW//\"/}
+        read -r -a SELECTED_PACKAGES <<< "$RAW"
+        ## Show permissions for Packages
+        echo "Permissions for Selected Packages: ${SELECTED_PACKAGES[*]}"
+        $SHOW_PERMISION ${SELECTED_PACKAGES[@]}
+        echo "If you get an error, please only select one..."
+    ;;
+    *)
+        echo "Exiting."
+        exit 0
+        ;;
+esac
+#just in case:)
+exit 0
--- scripts/5.Apps/03.Photo-&-Paint/switchero.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-flatpak install flathub io.gitlab.adhami3310.Converter
