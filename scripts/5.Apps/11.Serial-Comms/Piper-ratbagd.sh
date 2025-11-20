7b0647f82486c42689e51f28fc2e46c536e63732
squidnose <ozfiala@gmail.com>
2025-11-20 14:38:26 +0100

    Simplified Serial Comms thanks to LBAC

--- scripts/5.Apps/11.Serial-Comms/Piper-ratbagd.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-sudo xbps-install -y piper libratbag
-
--- scripts/5.Apps/11.Serial-Comms/chirp.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-sudo xbps-install -y chirp
-echo "Run Allow-Serial-Comms-Dialout.sh or add yourself to dialout to use"
